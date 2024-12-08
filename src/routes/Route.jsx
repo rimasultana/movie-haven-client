80a7b8b87a18cd83cb21af4c60f447e1b4c729b2
rimasultana <rimasultana20225@gmail.com>
2024-12-08 21:45:40 +0600

    last update add

--- src/routes/Route.jsx
+++ src/routes/Route.jsx
@@ -26,7 +26,7 @@ const router = createBrowserRouter([
         path: "/add-movie",
         element: (
           <PrivateRoute>
-            <AddMovie />,
+            <AddMovie />
           </PrivateRoute>
         ),
       },
